9aca02ca4c74d985e07c2c117a7c420ad0049877
Jeff Chiu <jwchiu82@gmail.com>
2021-02-22 15:44:47 -0800

    green pass index reducer test
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>"

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,16 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import cardListReducer from '../../reducers/card-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe('rootReducer', () => {
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, {type: null})).toEqual({
+      masterCardList: {},
+      formVisibleOnPage: false,
+    });
+  });
+  
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import cardListReducer from './card-list-reducer';
+import formVisibleReducer from './form-visible-reducer';
+import { combineReducers } from 'redux';
+// import { firestoreReducer } from 'redux-firestore';
+
+const rootReducer = combineReducers({
+  masterCardList: cardListReducer,
+  formVisibleOnPage: formVisibleReducer,
+  // firestore: firestoreReducer
+});
+
+export default rootReducer;
