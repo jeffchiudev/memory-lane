f05dd9b32b6c1daa19a11e9ad7dafedb72684254
Jeff Chiu <jwchiu82@gmail.com>
2021-02-22 14:03:34 -0800

    red fail for adding card reducer
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/__tests__/reducers/card-list-reducer.test.js
+++ src/__tests__/reducers/card-list-reducer.test.js
@@ -2,8 +2,32 @@ import cardListReducer from '../../reducers/card-list-reducer';
 
 describe('cardListReducer', () => {
 
+  let action;
+  const cardData = {
+    prompt: 'test prompt',
+    details: 'test details',
+    id: 1,
+  };
+  
   test('Should return default state if no action type passed into the reducer', () => {
     expect(cardListReducer({}, { type: null })).toEqual({});
   })
+
+  test('Should add a card to the masterCardList', () => {
+    const { prompt, details, id } = cardData;
+    action = {
+      type: 'ADD_CARD',
+      prompt,
+      details,
+      id
+    }
+    expect(cardListReducer({}, action)).toEqual({
+      [id]: {
+        prompt,
+        details,
+        id
+      }
+    });
+  });
   
 });
