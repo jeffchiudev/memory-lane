84713feacfef7020d2b7f338ca65cbf3990479e8
Jeff Chiu <jwchiu82@gmail.com>
2021-02-23 10:51:38 -0800

    red fail change of state of editing
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -4,4 +4,7 @@ describe ("editingReducer", () => {
   test('should return default state if no action type is recognized', () => {
     expect(editingReducer(false, { type: null })).toEqual(false);
   });
+  test('should toggle editing state to true', () => {
+    expect(editingReducer(false, { type: 'TOGGLE_EDIT'})).toEqual(true);
+  })
 })
