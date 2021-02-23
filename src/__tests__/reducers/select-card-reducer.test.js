4c553dc709289e6b06a039860300da10a5a8b07b
Jeff Chiu <jwchiu82@gmail.com>
2021-02-23 10:23:38 -0800

    red fail returning selected card
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/__tests__/reducers/select-card-reducer.test.js
+++ src/__tests__/reducers/select-card-reducer.test.js
@@ -1,7 +1,26 @@
 import selectCardReducer from '../../reducers/select-card-reducer';
 
 describe('selectCardReducer', () => {
+
+  let action;
+
+  const card = {
+    prompt: "test",
+    details: "test",
+    id: 1
+  }
+
   test('Should return default state if no types is recognized', () => {
     expect(selectCardReducer({}, {type: null})).toEqual({});
+  });
+  test('Should return selected card when SELECT_CARD is called', () => {
+    const { prompt, details, id } = card;
+    const action = {
+      types: 'SELECT_CARD',
+      prompt,
+      details,
+      id
+    }
+    expect(selectCardReducer({}, action)).toEqual(card)
   });
 });
