5a218f6ce9ac6371c60154e2da0c1d83df79d746
Jeff Chiu <jwchiu82@gmail.com>
2021-02-23 10:48:12 -0800

    red fail editingReducer tests for initial false state
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

---
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -0,0 +1,7 @@
+import editingReducer from '../../reducers/editing-reducer';
+
+describe ("editingReducer", () => {
+  test('should return default state if no action type is recognized', () => {
+    expect(editingReducer(false, { type: null })).toEqual(false);
+  });
+})
