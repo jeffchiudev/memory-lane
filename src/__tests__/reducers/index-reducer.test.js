a92576ce08ee4449d7ec0c45fa078f955f1ad34e
Jeff Chiu <jwchiu82@gmail.com>
2021-02-23 10:59:32 -0800

    green pass index reducer functionality

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,8 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import cardListReducer from '../../reducers/card-list-reducer';
+import selectCardReducer from '../../reducers/select-card-reducer';
+import editingReducer from '../../reducers/editing-reducer';
 
 let store = createStore(rootReducer);
 
@@ -10,7 +12,21 @@ describe('rootReducer', () => {
     expect(rootReducer({}, {type: null})).toEqual({
       masterCardList: {},
       formVisibleOnPage: false,
+      editingVisibleOnPage: false,
+      selectedCard: null
     });
   });
-
+  test('Check that initial state of cardListReducers matches root reducer', () => {
+    expect(store.getState().masterCardList).toEqual(cardListReducer(undefined, {type: null}));
+  });
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null}));
+  });
+  test('Check that initial state of selectedKegReducer matches root reducer', () => {
+    expect(store.getState().selectedCard).toEqual(selectCardReducer(undefined, {type: null}));
+  });
+  test('Check that initial state of editingReducer matches root reducer', () => {
+    expect(store.getState().editingVisibleOnPage).toEqual(editingReducer(undefined, { type: null}));
+  });
+  
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import cardListReducer from './card-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
 import { combineReducers } from 'redux';
+import selectCardReducer from './select-card-reducer';
+import editingReducer from './editing-reducer';
 // import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   masterCardList: cardListReducer,
   formVisibleOnPage: formVisibleReducer,
+  editingVisibleOnPage: editingReducer,
+  selectedCard: selectCardReducer
   // firestore: firestoreReducer
 });
 
